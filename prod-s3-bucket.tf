6e2203bf11a2f90f7efc603641e54c3319dd27e9
Emmanuel289 <emmirald@gmail.com>
2021-10-09 12:54:10 -0600

    Adopt default VPC

--- prod-s3-bucket.tf
+++ prod-s3-bucket.tf
@@ -3,8 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "okechukwu-tf-course"
   acl    = "private"
+}
+
+resource "aws_default_vpc" "default"{
+
 }
 
